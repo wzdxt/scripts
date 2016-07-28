e860c75905f4402a060affa405079d5be957eea7
戴鑫通 <xintong.dai@wenba100.com>
2016-07-28 12:35:00 +0800

    auto save

--- git/commit.sh
+++ git/commit.sh
@@ -4,6 +4,5 @@ echo "commit for $1"
 echo $1 \
 | grep -v "__jb_\w\w\w__" \
 | grep -o "${GOPATH}src/github\.com/wzdxt/[^/]*" \
-| xargs -I {} sh -c "git -C {} add . ; git -C {} commit -m 'auto save' ; git push origin head"
-
+| xargs -I {} sh -c "sleep 60 ; git -C {} add . ; git -C {} commit -m 'auto save' ; git push origin head"
 
