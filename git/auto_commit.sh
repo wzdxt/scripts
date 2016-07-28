b68a0d599eecd470e689128377eed77586ff0895
戴鑫通 <xintong.dai@wenba100.com>
2016-07-28 12:29:31 +0800

    auto save

--- git/auto_commit.sh
+++ git/auto_commit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-fswatch -x --batch-marker -e '\.git' -r $GOPATH/github.com/wzdxt/ \
+fswatch -x --batch-marker -e '\.git' -r $GOPATH/src/github.com/wzdxt/ \
 | xargs -I {} sh -c "./commit.sh {} auto" \
 #| grep -o "${GOPATH}src/github\.com/wzdxt/[^/]*" \
 #| xargs -I {} sh -c "git -C {} status"
