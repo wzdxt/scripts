51f65efaf29f733669a98b3f46f7e0b562317b11
戴鑫通 <xintong.dai@wenba100.com>
2016-07-28 12:04:53 +0800

    auto save

--- git/commit.sh
+++ git/commit.sh
@@ -2,8 +2,5 @@
 
 echo $1 \
 | grep -o "${GOPATH}src/github\.com/wzdxt/[^/]*" \
-| xargs -I {} sh -c "git -C {} add . ; git -C {} commit -m 'auto'"
-
-
-
+| xargs -I {} sh -c "git -C {} add . ; git -C {} commit -m 'auto save' ; git push"
 
