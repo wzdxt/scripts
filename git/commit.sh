7c658e73eca8bd3dd9d961c32f38f7974b8e020f
戴鑫通 <xintong.dai@wenba100.com>
2016-07-28 12:06:50 +0800

    auto save

--- git/commit.sh
+++ git/commit.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+echo "commit for $1"
 echo $1 \
 | grep -o "${GOPATH}src/github\.com/wzdxt/[^/]*" \
 | xargs -I {} sh -c "git -C {} add . ; git -C {} commit -m 'auto save' ; git push origin head"
